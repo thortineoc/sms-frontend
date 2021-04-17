22ec6ef466e87f0aded8d4d72ddb75268be918c5
Michał Stadryniak <stadryniak.michal@gmail.com>
2021-04-17 19:04:46 +0200

    keycloak installed and config files added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import LeftMenu from "./components/LeftMenu/LeftMenu";
 import ViewRouter from "./views/common/ViewRouter";
 import {BrowserRouter as Router} from "react-router-dom";
+import {ReactKeycloakProvider} from '@react-keycloak/web'
+
+import keycloak from './keycloak'
 
 const App = () => {
-    return (
+    return (<ReactKeycloakProvider
+        authClient={keycloak}>
         <div className="App">
             <Router>
                 <LeftMenu/>
                 <ViewRouter/>
             </Router>
         </div>
-    );
+    </ReactKeycloakProvider>);
 };
 
 export default App;
