eb5cc75028bebff04261849c07157eeef3a3ba07
Angelika <angkubicka99@gmail.com>
2021-04-21 13:24:53 +0200

    SMS-205 creating dialog component

--- src/views/common/ViewRouter.js
+++ src/views/common/ViewRouter.js
@@ -37,7 +37,7 @@ const ViewRouter = () => {
                 <TimetablesManagement />
             </Route>
             <Route path="/api/usermanagement-service/temp">
-                <CreateForm />
+
             </Route>
         </Switch>
     );
