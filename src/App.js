628324bb682874d14033cd66b8c5d676ddb2ad23
Michał Stadryniak <stadryniak.michal@gmail.com>
2021-04-23 09:17:22 +0200

    path updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ViewRouter from "./views/common/ViewRouter";
 import {BrowserRouter as Router} from "react-router-dom";
 import {ReactKeycloakProvider} from '@react-keycloak/web'
 
-import keycloak from './keycloak.js'
+import keycloak from './utilities/keycloak.js'
 
 const App = () => {
     return (<ReactKeycloakProvider
