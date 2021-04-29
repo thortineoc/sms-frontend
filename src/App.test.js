534a49d5abba9f9ef3972e666d0300bb2246a119
EKubicka <ekubicka@student.agh.edu.pl>
2021-04-30 00:11:45 +0200

    [SMS-221] moved initial render to external function

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {getQueriesForElement} from "@testing-library/react";
+
+const render = (component)=>{
+    const div = document.createElement('div');
+    ReactDOM.render(component, div);
+    return getQueriesForElement(div);
+}
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    render(<App />);
 });
