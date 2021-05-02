f6589be4997b58dc31508ce7c07d0c931f13f1b1
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-02 22:13:11 +0200

    [SMS-221] learn how to use render

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {getQueriesForElement} from "@testing-library/react";
-
-const render = (component)=>{
-    const div = document.createElement('div');
-    ReactDOM.render(component, div);
-    return getQueriesForElement(div);
-}
+import {getQueriesForElement, render, fireEvent, waitFor, screen} from "@testing-library/react";
+import UserManagement from "./views/usermanagement/layouts/UsersManagement/UserManagement";
 
 it('renders without crashing', () => {
     render(<App />);
+});
+
+it('can login', () => {
+    render(UserManagement);
+});
+
+it('goes to grades view', () => {
+   render(<App />);
+
+   fireEvent.click(screen.getByText('Grades'));
+});
+
+it('goes to homework view', () => {
+   render(<App />);
+
+   fireEvent.click(screen.getByText('Homework'));
+});
+
+it('goes to presences view', () => {
+   render(<App />);
+
+   fireEvent.click(screen.getByText('Presences'));
+});
+
+it('goes to timetables view', () => {
+   render(<App />);
+
+   fireEvent.click(screen.getByText('Timetables'));
 });
