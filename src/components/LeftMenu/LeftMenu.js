07f45f2a09dc127d0db7295796679c1368f27124
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-07 07:30:35 +0200

    [SMS-221] added ID property to open_drawer button

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -263,8 +263,9 @@ return (
                 <Toolbar>
                     {initialized && keycloak.authenticated ? (
                             <IconButton
+                                id='open_drawer'
                                 color="inherit"
-                                aria-label="open drawer"
+                                aria-label="open_drawer"
                                 onClick={handleDrawerOpen}
                                 edge="start"
                                 className={clsx(classes.menuButton, open && classes.hide)}
@@ -273,8 +274,9 @@ return (
                             </IconButton>) :
                         (
                             <IconButton
+                                id='open_drawer'
                                 color="inherit"
-                                aria-label="open drawer"
+                                aria-label="open_drawer"
                                 onClick={logOut}
                                 edge="start"
                                 className={clsx(classes.menuButton, open && classes.hide)}
