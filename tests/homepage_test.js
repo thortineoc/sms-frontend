9b3cd7ae8ffb7a0b6ab5ebe3ec9b8c20231a88de
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-07 07:48:38 +0200

    [SMS-221] tests passes locally

--- tests/homepage_test.js
+++ tests/homepage_test.js
@@ -1,19 +1,19 @@
 Feature('homepage');
 
-Scenario('is visible', ({ I }) => {
+Scenario('is visible', ({I}) => {
     //I.amOnPage('http://school-management-system.online:24020');
     I.amOnPage('http://localhost:3000/');
     I.see('School Management System');
     I.see('Welcome');
-    I.seeElement(locate({id:'open_drawer'}));
+    I.seeElement(locate({id: 'open_drawer'}));
 });
 
-Scenario('can log in', ({I}) => {
-   //I.amOnPage('http://school-management-system.online:24020');
-   I.amOnPage('http://localhost:3000/');
-   I.click({id:'open_drawer'})
-   I.see('Sign in to your account');
-   I.fillField('username', 'smsadmin');
-   I.fillField('password', 'smsadmin');
-   I.click('Sign In');
+Scenario('can make me log in', ({I}) => {
+    //I.amOnPage('http://school-management-system.online:24020');
+    I.amOnPage('http://localhost:3000/');
+    I.click({id: 'open_drawer'})
+    I.see('Sign in to your account');
+    I.fillField('username', 'smsadmin');
+    I.fillField('password', 'smsadmin');
+    I.click('Sign In');
 });
