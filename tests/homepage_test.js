3dae3da8609ffa0982874179242d9dbdbc621d54
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-07 07:32:20 +0200

    [SMS-221] created tests for homepage

--- tests/homepage_test.js
+++ tests/homepage_test.js
@@ -1,6 +1,19 @@
 Feature('homepage');
 
-Scenario('test something', ({ I }) => {
-    I.amOnPage('http://52.142.201.18:24020/');
+Scenario('is visible', ({ I }) => {
+    //I.amOnPage('http://school-management-system.online:24020');
+    I.amOnPage('http://localhost:3000/');
     I.see('School Management System');
+    I.see('Welcome');
+    I.seeElement(locate({id:'open_drawer'}));
+});
+
+Scenario('can log in', ({I}) => {
+   //I.amOnPage('http://school-management-system.online:24020');
+   I.amOnPage('http://localhost:3000/');
+   I.click({id:'open_drawer'})
+   I.see('Sign in to your account');
+   I.fillField('username', 'smsadmin');
+   I.fillField('password', 'smsadmin');
+   I.click('Sign In');
 });
