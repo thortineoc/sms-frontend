1e487679b110037a63267d11cb84b39a56fc336f
mateuszmulak <mateusz_mulak@icloud.com>
2021-05-08 23:03:49 +0200

    [SMS-247] created DatepickerWrapper and form in AssignEditHomeworkFrom

--- src/views/homework/HomeworkView.js
+++ src/views/homework/HomeworkView.js
@@ -1,7 +1,24 @@
+import React, {useState} from "react";
+import ButtonWrapper from "../../components/Button/ButtonWrapper";
+import Modal from "../../components/Modal/Modal";
+import AssignEditHomeworkForm from "./components/AssignEditHomeworkForm";
 
 
 const HomeworkView = (props) => {
-    return <p>Homework view</p>
+    const [state, setState] = useState(false);
+
+
+    return (
+        <div>
+        <ButtonWrapper label={"add"} onClick={() => setState(true)}/>
+        <Modal isOpen={state} setIsOpen={setState}>
+            <AssignEditHomeworkForm
+            type={"ADD"}
+            subjects={["Polish", "Math"]}
+            />
+        </Modal>
+        </div>
+    )
 }
 
 export default HomeworkView;
