17f7f0153b5bfc04cc2ae2298e503bfab5ca04b1
Angelika <angkubicka99@gmail.com>
2021-05-09 22:00:08 +0200

    SMS-245 tree structure

--- src/views/homework/HomeworksList/HomerworksList.js
+++ src/views/homework/HomeworksList/HomerworksList.js
@@ -31,32 +31,28 @@ const HomeworksList = () => {
     return (
         <TreeView
             className={classes.root}
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
         >
             {
-                mockData.map((item, index) => (
-                    <TreeItem nodeId={index} label={Object.keys(item)}>
-                        <TreeItem nodeId={index*2+5} label="hheheh" />
+
+                mockData.map((item, subjectIndex) => (
+                    <TreeItem nodeId={subjectIndex} label={Object.keys(item)}>
+                        {Object.keys(item).map((subjectKey) => (
+                            item[subjectKey].map((groupObj, groupIndex) => (
+                                <TreeItem nodeId={1000 + groupIndex} label={Object.keys(groupObj)}>
+                                    {Object.keys(groupObj).map((groupKey) => (
+                                        groupObj[groupKey].map((homework, homeworkIndex) => (
+                                                <TreeItem nodeId={2000 + homeworkIndex} label={homework}/>
+                                            )
+                                        )
+                                    ))}
+                                </TreeItem>
+                            ))
+                        ))}
                     </TreeItem>
                 ))
             }
-            {/*
-            <TreeItem nodeId="1" label="Applications">
-                <TreeItem nodeId="2" label="Calendar" />
-                <TreeItem nodeId="3" label="Chrome" />
-                <TreeItem nodeId="4" label="Webstorm" />
-            </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-                <TreeItem nodeId="10" label="OSS" />
-                <TreeItem nodeId="6" label="Material-UI">
-                    <TreeItem nodeId="7" label="src">
-                        <TreeItem nodeId="8" label="index.js" />
-                        <TreeItem nodeId="9" label="tree-view.js" />
-                    </TreeItem>
-                </TreeItem>
-            </TreeItem>
-            */}
         </TreeView>
     );
 }
