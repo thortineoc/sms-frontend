244181127052a07f2d048417a8520bff84c58fad
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-10 14:44:09 +0200

    [SMS-221] set tests to server

--- tests/usermanagement_test.js
+++ tests/usermanagement_test.js
@@ -1,9 +1,9 @@
 Feature('usermanagement');
 
-Scenario('can login to admin account and logout', ({ I }) => {
+Scenario('can login to admin account and logout', ({I}) => {
     //homepage
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:3000/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
     I.see('Sign in to your account');
     I.fillField('username', 'smsadmin');
@@ -27,11 +27,10 @@ Scenario('can login to admin account and logout', ({ I }) => {
     I.see('Welcome');
 });
 
-
-Scenario('can create and delete group', ({ I }) => {
+Scenario('can create and delete group', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:3000/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 'smsadmin');
@@ -47,20 +46,19 @@ Scenario('can create and delete group', ({ I }) => {
     //create group
     I.click({id: "manage_groups"});
     I.dontSee('TEST_EG');
-    I.fillField(locate({id:'add_new'}), 'TEST_EG');
-    I.click({id:'submit_new'});
+    I.fillField(locate({id: 'add_new'}), 'TEST_EG');
+    I.click({id: 'submit_new'});
     I.see('TEST_EG');
 
     //delete group
-    I.click({id:"delete_TEST_EG"});
+    I.click({id: "delete_TEST_EG"});
     I.dontSee('TEST_EG');
 });
 
-
-Scenario('can create and delete student', ({ I }) => {
+Scenario('can create and delete student', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:3000/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 'smsadmin');
@@ -79,10 +77,9 @@ Scenario('can create and delete student', ({ I }) => {
     I.dontSee('FrontTestLN');
 
     //create FrontTest
-    I.click({id:"add_user"});
+    I.click({id: "add_user"});
     I.see('Create new student');
     I.fillField('firstName', 'FrontTest');
-    //I.fillField('middleName', 'FrontTestMN');
     I.fillField('lastName', 'FrontTestLN');
     I.fillField('pesel', '00000000000');
     I.fillField('email', 'fronttest@sms.com');
@@ -94,20 +91,19 @@ Scenario('can create and delete student', ({ I }) => {
     I.see('FrontTestLN');
 
     //delete FrontTest
-    I.click({id:"s_00000000000"});
-    I.waitForClickable({id:'delete'});
+    I.click({id: "s_00000000000"});
+    I.waitForClickable({id: 'delete'});
     I.see('Personal information');
-    I.click({id:'delete'});
+    I.click({id: 'delete'});
     I.see('Are you sure that you want to delete this account?');
-    I.click({id:'yes'});
+    I.click({id: 'yes'});
     I.dontSee('FrontTestLN');
 });
 
-
-Scenario('can filter students', ({ I }) => {
+Scenario('can filter students', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:3000/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     //login
@@ -123,23 +119,22 @@ Scenario('can filter students', ({ I }) => {
 
     //filter students by email
     I.see('2A');
-    I.click({id:'filter_users'});
+    I.click({id: 'filter_users'});
     //I.fillField('email', '@sms');
     I.fillField('group', '1B');
-    I.click({id:'apply'});
+    I.click({id: 'apply'});
     I.dontSee('2A');
 
     //reset filters
-    I.click({id:'filter_users'});
-    I.click({id:'reset'});
+    I.click({id: 'filter_users'});
+    I.click({id: 'reset'});
     I.see('2A');
 });
 
-
-Scenario('can change displayed columns', ({ I }) => {
+Scenario('can change displayed columns', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:3000/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     //login
@@ -165,17 +160,17 @@ Scenario('can change displayed columns', ({ I }) => {
     I.dontSee('ID');
 
     //completly change displayed columns
-    I.click({id:'columns'});
-    I.click({name:'0'});        //First Name
-    I.click({name:'1'});        //Middle Name
-    I.click({name:'2'});        //Last Name
-    I.click({name:'3'});        //E-mail
-    I.click({name:'4'});        //Phone
-    I.click({name:'5'});        //Group
-    I.click({name:'6'});        //Pesel
-    I.click({name:'7'});        //Username
-    I.click({name:'8'});        //ID
-    I.click({id:"apply"});
+    I.click({id: 'columns'});
+    I.click({name: '0'});        //First Name
+    I.click({name: '1'});        //Middle Name
+    I.click({name: '2'});        //Last Name
+    I.click({name: '3'});        //E-mail
+    I.click({name: '4'});        //Phone
+    I.click({name: '5'});        //Group
+    I.click({name: '6'});        //Pesel
+    I.click({name: '7'});        //Username
+    I.click({name: '8'});        //ID
+    I.click({id: "apply"});
 
     //check if displayed columns changed
     I.dontSee('First Name');
