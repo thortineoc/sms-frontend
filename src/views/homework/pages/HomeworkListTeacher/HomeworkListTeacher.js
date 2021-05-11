1c1db41070b0cdbb15ff714980441fb8cf983220
thortineoc <angkubicka99@gmail.com>
2021-05-11 20:03:37 +0200

    SMS-259 made homework list bigger

--- src/views/homework/pages/HomeworkListTeacher/HomeworkListTeacher.js
+++ src/views/homework/pages/HomeworkListTeacher/HomeworkListTeacher.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles, ThemeProvider} from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -10,27 +10,32 @@ import './HomeworkListTeacher.css';
 import Modal from "../../../../components/Modal/Modal";
 import AssignEditHomeworkForm from "../../components/AssignEditHomeworkForm/AssignEditHomeworkForm";
 import { v4 as uuidv4 } from 'uuid';
+import Typography from "@material-ui/core/Typography";
 
 const mockData = [
     {'geography': [
             {'3A': [{
                 'id': 12345679,
-                'title': 'write about caves'
+                'title': 'write about caves',
+                    'date': '12-05-2021'
             }]},
             {'2B' : [{
                 'id': 12345680,
-                'title': 'write about rivers'
+                'title': 'write about rivers',
+                    'date': '12-06-2021'
             },
                     {
                         'id': 12345688,
-                        'title': 'write about sees'
+                        'title': 'write about sees',
+                        'date': '22-05-2021'
                     }]}
         ]
     },
     {'english': [
             {'1C' : [
                     {'id': 123456781,
-                        'title': 'write an email'
+                        'title': 'write an email',
+                        'date': '23-05-2021'
                     }]}
         ]
     }
@@ -38,11 +43,59 @@ const mockData = [
 
 const useStyles = makeStyles({
     root: {
-        height: 240,
+        height: 2000,
         flexGrow: 1,
-        maxWidth: 400,
+        maxWidth: 450,
     },
 });
+
+const useTreeItemStyles = makeStyles((theme) => ({
+    label: {
+        fontWeight: 'inherit',
+        color: 'inherit',
+    },
+    labelRoot: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(1, 0),
+        fontSize: 18
+    },
+    labelText: {
+        fontWeight: 'inherit',
+        flexGrow: 1,
+    },
+    labelInfo: {
+        color: 'gray'
+    }
+}));
+
+function StyledTreeItem(props) {
+    const classes = useTreeItemStyles();
+    const { labelText, labelInfo, bgColor, ...other } = props;
+
+    return (
+        <TreeItem
+            label={
+                <div className={classes.labelRoot}>
+                    <Typography variant="" className={classes.labelText}>
+                        {labelText}
+                    </Typography>
+                    <Typography className={classes.labelInfo}>
+                        {labelInfo}
+                    </Typography>
+                </div>
+            }
+            style={{
+                '--tree-view-bg-color': bgColor,
+            }}
+            classes={{
+                label: classes.label,
+            }}
+            {...other}
+        />
+    );
+}
+
 
 const HomeworksList = () => {
     const classes = useStyles();
@@ -53,27 +106,31 @@ const HomeworksList = () => {
 
             <TreeView
                 className={classes.root}
-                defaultCollapseIcon={<ExpandMoreIcon/>}
-                defaultExpandIcon={<ChevronRightIcon/>}
+                defaultCollapseIcon={<ExpandMoreIcon style={{color: 'navy'}}/>}
+                defaultExpandIcon={<ChevronRightIcon style={{color: 'rgb(33,150,243)'}}/>}
             >
                 {
                     mockData.map((item) => (
-                        <TreeItem nodeId={uuidv4()} label={Object.keys(item)}>
+                        <StyledTreeItem nodeId={uuidv4()} labelText={Object.keys(item)} >
                             {Object.keys(item).map((subjectKey) => (
                                 item[subjectKey].map((groupObj, groupIndex) => (
-                                    <TreeItem nodeId={uuidv4()} label={Object.keys(groupObj)}>
+                                    <StyledTreeItem nodeId={uuidv4()} labelText={Object.keys(groupObj)}>
                                         {Object.keys(groupObj).map((groupKey) => (
-                                            groupObj[groupKey].map((homework, homeworkIndex) => (
+                                            groupObj[groupKey].map((homework) => (
                                                     <Link to={`/api/homework/${homework['id']}`}>
-                                                        <TreeItem nodeId={homework['id']} label={homework['title']}/>
+                                                        <StyledTreeItem nodeId={homework['id']}
+                                                                        labelText={homework['title']}
+                                                                        labelInfo={homework['date']}
+
+                                                        />
                                                     </Link>
                                                 )
                                             )
                                         ))}
-                                    </TreeItem>
+                                    </StyledTreeItem>
                                 ))
                             ))}
-                        </TreeItem>
+                        </StyledTreeItem>
                     ))
                 }
             </TreeView>
@@ -90,3 +147,5 @@ const HomeworksList = () => {
 }
 
 export default HomeworksList;
+
+
