34947146097b62d373931fac0ffcf7f7441c0aa5
mateuszmulak <mateusz_mulak@icloud.com>
2021-05-17 13:25:08 +0200

    [SMS-257] added backend

--- src/views/homework/components/HomeworkDetailsAndResponses/HomeworkDetailsAndResponses.js
+++ src/views/homework/components/HomeworkDetailsAndResponses/HomeworkDetailsAndResponses.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from "react";
 import ButtonWrapper from "../../../../components/Button/ButtonWrapper";
 import Modal from "../../../../components/Modal/Modal";
-import AssignEditHomeworkForm from "../AssignEditHomeworkForm/AssignEditHomeworkForm";
+import AnswersTable from "../AnswersTable/AnswersTable";
 import "./HomeworkDetailsAndResponses.css"
 import DeleteDialog from "../DeleteDialog/DeleteDialog";
-import AssignmentsTable from "../AssigmentsTable/AssignmentsTable";
 import getKeycloakRoles from "../../../../utilities/GetRoles";
 import {useKeycloak} from "@react-keycloak/web";
 import {Form, Formik} from "formik";
@@ -17,10 +16,55 @@ import getKeycloakSubjects from "../../../../utilities/GetSubjects";
 
 const homeworkData = {
     title: "Example homework",
+    group: "3A",
+    subject: "Polish",
+    deadline: "10/10/2021",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    subject: "Math",
-    group: "1A",
-    deadline: "10/10/2021"
+    toEvaluate: true,
+    answers: [{
+        review: "REVIEW lorem ipsum dolor sit amet",
+        files: [{
+            filename: "filename_xd",
+            uri: "/what/is/it"
+        },
+            {
+                filename: "filename_xd2",
+                uri: "/what/is/it2"
+            }],
+        grade: {
+            grade: "4"
+        },
+        user: {
+            id: "rafal1",
+            firstName: "Rafal",
+            lastName: "Carlos"
+        },
+        lastUpdatedTime: "21-21-2021",
+        createdTime: "11-21-2021"
+    },
+        {
+            review: "REVIEW lorem ipsum dolor sit amet",
+            files: [{
+                id: "1234",
+                filename: "filename_xd",
+                uri: "/what/is/it"
+            },
+                {
+                    filename: "filename_xd2",
+                    uri: "/what/is/it2"
+                }],
+            // grade: {
+            //     grade: "4"
+            // },
+            user: {
+                id: "rafal2",
+                firstName: "Rafal",
+                lastName: "Brzozowski"
+            },
+            lastUpdatedTime: "21-21-2021",
+            createdTime: "11-21-2021"
+        }
+    ],
 }
 
 const HomeworkDetailsAndResponses = (props) => {
@@ -179,7 +223,11 @@ const HomeworkDetailsAndResponses = (props) => {
         <div>
             {showEdit ? editPage() : detailsPage()}
             {role==="TEACHER" &&
-            <AssignmentsTable/>}
+            <AnswersTable
+                answers={homeworkData.answers}
+                subject={homeworkData.subject}
+                group={homeworkData.group}
+                toGrade={homeworkData.toEvaluate}/>}
             <Modal isOpen={showDeleteDialog} setIsOpen={setShowDeleteDialog}>
                 <DeleteDialog setDisplayDialog={setShowDeleteDialog}/>
             </Modal>
