f51588b09c867982cfe78361ecbe2656830df0b2
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-18 00:11:39 +0200

    [SMS-261] changed grades div id

--- src/views/grades/components/Grade/Grade.js
+++ src/views/grades/components/Grade/Grade.js
@@ -41,7 +41,7 @@ const Grade = ({role, value, type, setRefresh}) => {
                               subject={value.subject}
             >
                 <div className="Grade-wrapper" onClick={() => handleClick(value)}>
-                    <div id={"grade_"+value.grade} className={classes}>
+                    <div id={"grade_"+value.id} className={classes}>
                         {result}
                     </div>
                 </div>
