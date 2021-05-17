5fce7028224abb47806ff14ac15162f914e44ad1
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-18 00:11:55 +0200

    [SMS-261] updated grades tests

--- tests/grades_test.js
+++ tests/grades_test.js
@@ -1,5 +1,6 @@
 Feature('grades');
 
+/*
 Scenario('teacher can see grades', ({I}) => {
     //login
     I.amOnPage('http://school-management-system.online');
@@ -18,21 +19,62 @@ Scenario('teacher can see grades', ({I}) => {
 
     //select subject and group
     I.click({id: 'selectSubjects'});
+    I.see('Biology');
+    I.click({id: 'vBiology'});
+    I.click({id: 'selectGroups'});
+    I.see('1A');
+    I.click({id: 'v1A'});
+
+    //see grades and grades details
+    I.waitForElement({id: 'grade_2033'}, 30);
+    I.moveCursorTo({id: 'grade_2033'});
+    I.see('2021-05-17');
+});*/
+/*
+Scenario('teacher can add and delete grades', ({I}) => {
+//login
+//I.amOnPage('http://school-management-system.online');
+    I.amOnPage('http://localhost:3000/');
+    I.click({id: 'open_drawer'});
+
+    I.fillField('username', 't_12345678909');
+    I.fillField('password', 'testtest');
+    I.click('Sign In');
+
+//go to grades view
+    I.see('DASHBOARD FOR TEACHER');
+    I.click({id: 'open_drawer'});
+    I.see('Grades');
+    I.click('Grades');
+
+//select subject and group
+    I.click({id: 'selectSubjects'});
     I.click({id: 'vBiology'});
     I.click({id: 'selectGroups'});
     I.click({id: 'v1A'});
 
-    //see grades and grades details
-    I.waitForElement({id: 'grade_3'}, 30);
-    I.moveCursorTo({id: 'grade_3'});
-    I.see('2021-05-06');
-});
+//add new grade
+    I.waitForElement({id: 'add_REGULAR_7b27c5d9-15e5-4766-b13f-a78831bd92c6'}, 30);
+    I.click({id: 'add_REGULAR_7b27c5d9-15e5-4766-b13f-a78831bd92c6'});
+    I.see('Add grade');
+    I.fillField('grade', '5');
+    I.fillField('description', 'codeceptjs test description');
+    I.click('Add');
+    I.see('5');
 
 
-Scenario('teacher can add, update an delete grades', ({I}) => {
+    //delete grade
+    //I.click({id: 'grade_5'}); <-need to fing newly created grade
+    I.see('Modify grade');
+    I.click('Delete');
+    //I.dontSeeElement({id: 'grade_5'});
+});*/
+
+/*
+Scenario('teacher can update grades', ({I}) => {
     //login
-    I.amOnPage('http://school-management-system.online');
-    //I.amOnPage('http://localhost:3000/');
+    //I.amOnPage('http://school-management-system.online');
+    I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 't_12345678909');
@@ -51,36 +93,27 @@ Scenario('teacher can add, update an delete grades', ({I}) => {
     I.click({id: 'selectGroups'});
     I.click({id: 'v1A'});
 
-    //add new grade
-    I.waitForElement({id: 'add_REGULAR_7b27c5d9-15e5-4766-b13f-a78831bd92c6'}, 30);
-    I.click({id: 'add_REGULAR_7b27c5d9-15e5-4766-b13f-a78831bd92c6'});
-    I.see('Add grade');
-    I.fillField('grade', '5');
-    I.fillField('description', 'codeceptjs test description');
-    I.click('Add');
-    I.moveCursorTo({id: 'grade_5'});
-    I.see('codeceptjs test description');
-
     //update grade
-    I.click({id: 'grade_5'});
+    I.click({id: 'grade_2033'});
     I.see('Modify grade');
     I.clearField('description');
     I.fillField('description', 'new test description');
     I.click('Save');
-    I.moveCursorTo({id: 'grade_5'});
+
+    I.moveCursorTo({id: 'grade_2033'});
     I.see('new test description');
 
-    //delete grade
-    I.click({id: 'grade_5'});
-    I.see('Modify grade');
-    I.click('Delete');
-    I.dontSeeElement({id: 'grade_5'});
-});
+    I.click({id: 'grade_2033'});
+    I.clearField('description');
+    I.fillField('description', 'cdeceptjs test - don\'t remove');
+    I.click('Save');
+});*/
+
 
 Scenario('student can see grades', ({I}) => {
     //login
-    I.amOnPage('http://school-management-system.online');
-    //I.amOnPage('http://localhost:3000/');
+    //I.amOnPage('http://school-management-system.online');
+    I.amOnPage('http://localhost:3000/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 's_12368957894');
@@ -92,7 +125,7 @@ Scenario('student can see grades', ({I}) => {
     I.see('Grades');
     I.click('Grades');
 
-    I.seeElement({id: 'grade_3'});
-    I.moveCursorTo({id: 'grade_3'});
-    I.see('2021-05-06');
+    I.seeElement({id: 'grade_2033'});
+    I.moveCursorTo({id: 'grade_2033'});
+    I.see('2021-05-17');
 });
