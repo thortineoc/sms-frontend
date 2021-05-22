e2bb3dd25906a168354ab5253716d6820977eddd
thortineoc <angkubicka99@gmail.com>
2021-05-22 23:08:16 +0200

    SMS-278 timetable logic

--- src/views/timetables/components/TimetableCell/TimetableCell.js
+++ src/views/timetables/components/TimetableCell/TimetableCell.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './TimetableCell.css';
 
+const getRowNumber = (id) => {
+    let pos = id.indexOf('L');
+    return Number.parseInt(id.slice(0, pos));
+}
+
+const getColNumber = (id) => {
+    let start = id.indexOf('L') + 1;
+    let end = id.indexOf('R');
+    return Number.parseInt(id.slice(start, end));
+}
+
 const TimetableCell = ({id}) => {
+    console.log(getColNumber(id))
+
     return (
         <td className="TimetableCell">
-            {id}
+            { getColNumber(id) === 0 ? (
+                8.00
+            ) : (
+              "lsls"
+            )}
         </td>
     );
 };
