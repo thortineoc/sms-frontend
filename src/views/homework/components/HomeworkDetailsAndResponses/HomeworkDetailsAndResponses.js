814a1864ffb5003a1fa2f298b0c0ba99156ca29e
EKubicka <ekubicka@student.agh.edu.pl>
2021-05-23 22:05:31 +0200

    [SMS-282] added some more id's

--- src/views/homework/components/HomeworkDetailsAndResponses/HomeworkDetailsAndResponses.js
+++ src/views/homework/components/HomeworkDetailsAndResponses/HomeworkDetailsAndResponses.js
@@ -48,7 +48,7 @@ const HomeworkDetailsAndResponses = (props) => {
     const axiosInstance = useAxios('http://52.142.201.18:24020/');
     const {keycloak, initialized} = useKeycloak();
     const [role, setRole] = useState("");
-    const[error, setError] = useState("");
+    const [error, setError] = useState("");
     const [groups, setGroups] = useState([]);
     const [allSubjects, setAllSubjects] = useState([]);
     const [homeworkData, setHomeworkData] = useState(null);
@@ -172,37 +172,37 @@ const HomeworkDetailsAndResponses = (props) => {
                 <ButtonWrapper label={"Delete"} onClick={() => setShowDeleteDialog(true)} className="HomeworkDetails__button"/>}
                 <h3>Homework details</h3>
 
-                <div id="homework_title" className="DetailsHomework__field">
+                <div className="DetailsHomework__field">
                     <div className="DetailsHomework__label">Title</div>
-                    <div className="DetailsHomework__data" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
+                    <div id="homework_title" className="DetailsHomework__data" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
                         {homeworkData.title}
                     </div>
                 </div>
 
-                <div id="homework_description" className="DetailsHomework__field">
+                <div className="DetailsHomework__field">
                     <div className="DetailsHomework__label">Description</div>
-                    <div className="DetailsHomework__data" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit)? {cursor: "pointer"} : undefined}>
+                    <div id="homework_description" className="DetailsHomework__data" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit)? {cursor: "pointer"} : undefined}>
                         {homeworkData.description}
                     </div>
                 </div>
 
-                <div id="homework_group" className="DetailsHomework__field">
+                <div className="DetailsHomework__field">
                     <div className="DetailsHomework__label">Group</div>
-                    <div className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
+                    <div id="homework_group" className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
                         {homeworkData.group}
                     </div>
                 </div>
 
-                <div id="homework_subject" className="DetailsHomework__field">
+                <div className="DetailsHomework__field">
                     <div className="DetailsHomework__label">Subject</div>
-                    <div className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
+                    <div id="homework_subject" className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
                         {homeworkData.subject}
                     </div>
                 </div>
 
-                <div id="homework_deadline" className="DetailsHomework__field">
+                <div className="DetailsHomework__field">
                     <div className="DetailsHomework__label">Deadline</div>
-                    <div className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
+                    <div id="homework_deadline" className="DetailsHomework__data_small" onClick={(role==="TEACHER" && allowEdit) ? handleClick : undefined } style={(role==="TEACHER" && allowEdit) ? {cursor: "pointer"} : undefined}>
                         {homeworkData.deadline  ? (homeworkData.deadline.split("T")[0]) : ""}
                     </div>
                 </div>
