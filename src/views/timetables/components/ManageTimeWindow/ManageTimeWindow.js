d588e26abeab0a3ce835bb5c5f5a8ef63e3c5137
mateuszmulak <mateusz_mulak@icloud.com>
2021-05-25 17:59:05 +0200

    [SMS-288] component for config

--- src/views/timetables/components/ManageTimeWindow/ManageTimeWindow.js
+++ src/views/timetables/components/ManageTimeWindow/ManageTimeWindow.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import useAxios from "../../../../utilities/useAxios";
+import smsConfig from "../../../../utilities/configuration";
+import callBackendGet from "../../../../utilities/CallBackendGet";
 
 const ManageTimeWindow = ({setIsOpen}) => {
+    const [code, setCode] = useState(null);
+    const axiosInstance = useAxios(smsConfig.haproxyUrl);
+
+    const fetchData = () => {
+        callBackendGet(axiosInstance, "/timetable-service/config", null)
+            .then(response => {
+                setCode(response.status);
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
 
     return (
         <div>
             <h3>Set breaks length</h3>
+            <p>{code}</p>
         </div>
     );
 };
