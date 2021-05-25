6a9b022da063f104a6fbce31ff01f492ee2444b0
mateuszmulak <mateusz_mulak@icloud.com>
2021-05-26 00:36:56 +0200

    [SMS-288] component for config

---
+++ src/components/TimePickerWrapper/TimePickerWrapper.js
@@ -0,0 +1,48 @@
+import 'date-fns';
+import React, {useEffect} from 'react';
+import DateFnsUtils from '@date-io/date-fns'
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import {useFormikContext} from "formik";
+import {createMuiTheme} from "@material-ui/core";
+import {blue} from "@material-ui/core/colors";
+import {ThemeProvider} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: blue
+    }
+});
+
+export default function TimePickerWrapper({name, label}) {
+    const [selectedDate, setSelectedDate] = React.useState(null);
+    const { setFieldValue } = useFormikContext();
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+        setFieldValue(name, date)
+    };
+
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <KeyboardTimePicker
+                    margin="normal"
+                    id={name}
+                    ampm={false}
+                    label={label}
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change time',
+                    }}
+                />
+            </MuiPickersUtilsProvider>
+        </ThemeProvider>
+    );
+}
--- src/views/timetables/components/ManageTimeWindow/ManageTimeWindow.js
+++ src/views/timetables/components/ManageTimeWindow/ManageTimeWindow.js
@@ -2,15 +2,23 @@ import React, {useEffect, useState} from 'react';
 import useAxios from "../../../../utilities/useAxios";
 import smsConfig from "../../../../utilities/configuration";
 import callBackendGet from "../../../../utilities/CallBackendGet";
+import {Form, Formik} from "formik";
+import TextFieldWrapper from "../../../../components/TextFieldWrapper/TextFieldWrapper";
+import ButtonWrapper from "../../../../components/Button/ButtonWrapper";
+import callBackendPost from "../../../../utilities/CallBackendPost";
+import TimePickerWrapper from "../../../../components/TimePickerWrapper/TimePickerWrapper";
+import {Grid} from "@material-ui/core";
+
 
 const ManageTimeWindow = ({setIsOpen}) => {
-    const [code, setCode] = useState(null);
+    const [response, setResponse] = useState(null);
     const axiosInstance = useAxios(smsConfig.haproxyUrl);
 
     const fetchData = () => {
         callBackendGet(axiosInstance, "/timetable-service/config", null)
             .then(response => {
-                setCode(response.status);
+                setResponse(response);
+                console.log(response)
             })
             .catch(error => console.log(error))
     }
@@ -19,13 +27,121 @@ const ManageTimeWindow = ({setIsOpen}) => {
         fetchData();
     }, []);
 
+    const onSubmit = (values) => {
+        callBackendPost(axiosInstance, "/timetable-service/config", values)
+            .then(response => {
+                console.log(response)
+                fetchData()
+            })
+            .catch(error => console.log(error))
+    }
+
+    const onSubmit1 = (values) => {
+        console.log(values)
+        let data = {}
+        data.lessonCount = values.lessonCount
+        data.config = []
+        for(let i = 0; i<values.lessonCount; i++){
+            let temp = {}
+            temp.startTime = "08:00"
+            temp.endTime = "09:00"
+            data.config.push(temp)
+        }
+        console.log(data)
+    }
+
+    const getInitialValues = (lessonCount) =>{
+        const init = {}
+        init.lessonCount = lessonCount;
+        for(let i=0; i<lessonCount; i++){
+            init["startDate"+i]=""
+            init["endDate"+i]=""
+        }
+        return init
+    }
 
     return (
         <div>
             <h3>Set breaks length</h3>
-            <p>{code}</p>
-        </div>
-    );
-};
+            <Formik
+                enableReinitialize
+                initialValues={((response && response.status === 200) ? response.data : {lessonCount: 0})}
+                //validationSchema={validationSchema}
+                validateOnChange={false}
+                onSubmit={onSubmit}
+            >
+                {
+                    formik => {
+                        return (
+                            <Form>
+                                <div>
+                                    {formik.errors && formik.errors.submit &&
+                                    <div className="error">{formik.errors.submit}</div>}
+                                    <TextFieldWrapper
+                                        id="lessonCount"
+                                        label={"Number of lessons"}
+                                        name={"lessonCount"}
+                                        type="text"
+                                    />
+                                </div>
+
+                                <ButtonWrapper
+                                    type="submit"
+                                    label="Save"
+                                />
+
+                            </Form>
+                        )
+                    }
+                }
+            </Formik>
+
+            {response && response.data.lessonCount > 0 ? (
+                <Formik
+                    enableReinitialize
+                    initialValues={getInitialValues(response.data.lessonCount)}
+                    //validationSchema={validationSchema}
+                    validateOnChange={false}
+                    onSubmit={onSubmit1}
+                >
+                    {
+                        formik => {
+                            return (
+                                <Form>
+                                    <div>
+                                        {formik.errors && formik.errors.submit &&
+                                        <div className="error">{formik.errors.submit}</div>}
+
+                                        {[...Array(response.data.lessonCount)].map((x, i) =>
+                                            <Grid container justify="space-around">
+                                                <TimePickerWrapper
+                                                    name={"startDate"+(i+1)}
+                                                    label={"Lesson " + (i+1) + " start time"}
+                                                />
+                                                <TimePickerWrapper
+                                                    name={"endDate"+(i+1)}
+                                                    label={"Lesson " + (i+1) + " end time"}
+                                                />
+                                            </Grid>
+                                        )}
+
+
+
+                                        <ButtonWrapper
+                                            type="submit"
+                                            label="Save"
+                                        />
+                                    </div>
+                                </Form>
+                            )
+                        }
+                        }
+                        </Formik>
+
+
+                        ) : (<></>)}
+                </div>
+            );
+            };
 
 export default ManageTimeWindow;
