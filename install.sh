5638cf223fbf68f86affcda823ed90ef6ddb0643
Michal Stadryniak <michal.stadryniak@gmail.com>
2021-05-29 14:05:28 +0200

    [sms-297]install script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
+sed -i 's/52.142.201.18/localhost/g' public/keycloak.json
+sed -i 's/52.142.201.18/localhost/g' src/configuration.json
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo apt-get update
 sudo apt-get -y install npm
 sudo npm install -g serve
 npm install
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
 npm run build
 
