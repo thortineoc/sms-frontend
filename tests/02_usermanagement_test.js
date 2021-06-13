fce02fc60d053182ef70c7bef40847e11457daa9
EKubicka <ekubicka@student.agh.edu.pl>
2021-06-13 19:41:49 +0200

    [SMS-290] repaired usermanagement tests

--- tests/02_usermanagement_test.js
+++ tests/02_usermanagement_test.js
@@ -2,8 +2,8 @@ Feature('usermanagement');
 
 Scenario('can login to admin account and logout', ({I}) => {
     //homepage
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:24010/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
     I.click({id: 'open_drawer'});
     I.see('Sign in to your account');
     I.fillField('username', 'smsadmin');
@@ -27,10 +27,10 @@ Scenario('can login to admin account and logout', ({I}) => {
     //I.see('Welcome');
 });
 
-Scenario('can create and delete group', ({I}) => {
+Scenario('can create group', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:24010/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 'smsadmin');
@@ -49,16 +49,19 @@ Scenario('can create and delete group', ({I}) => {
     I.fillField(locate({id: 'add_new'}), 'TEST_EG');
     I.click({id: 'submit_new'});
     I.see('TEST_EG');
+    I.fillField(locate({id: 'add_new'}), 'TEST_FG');
+    I.click({id: 'submit_new'});
+    I.see('TEST_FG');
 
     //delete group
-    I.click({id: "delete_TEST_EG"});
-    I.dontSee('TEST_EG');
+/*    I.click({id: "delete_TEST_EG"});
+    I.dontSee('TEST_EG');*/
 });
 
-Scenario('can create and delete student', ({I}) => {
+Scenario('can create student', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:24010/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
     I.click({id: 'open_drawer'});
 
     I.fillField('username', 'smsadmin');
@@ -85,26 +88,24 @@ Scenario('can create and delete student', ({I}) => {
     I.fillField('email', 'fronttest@sms.com');
     I.click('Submit');
 
+    I.click({id: "add_user"});
+    I.see('Create new student');
+    I.fillField('firstName', 'Front1Test');
+    I.fillField('lastName', 'Front1TestLN');
+    I.fillField('pesel', '11111111111');
+    I.fillField('email', 'front1test@sms.com');
+    I.click('Submit');
     //search for FrontTest
-    //I.fillField('Search', 'FrontTest');
-    //I.pressKey('Enter');
+    I.fillField('Search', 'FrontTest');
+    I.pressKey('Enter');
     I.see('FrontTestLN');
 
-    //delete FrontTest
-    I.click({id: "s_00000000000"});
-    I.waitForClickable({id: 'delete'});
-    I.see('Personal information');
-    I.click({id: 'delete'});
-    I.see('Are you sure that you want to delete this account?');
-    I.click({id: 'yes'});
-    I.wait(5);
-    I.dontSee('FrontTestLN');
 });
 
 Scenario('can filter students', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:24010/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
     I.click({id: 'open_drawer'});
 
     //login
@@ -119,24 +120,28 @@ Scenario('can filter students', ({I}) => {
     I.see('Student management');
 
     //filter students by email
-    I.see('2A');
     I.click({id: 'filter_users'});
     //I.fillField('email', '@sms');
-    I.fillField('group', '1B');
+    I.fillField('firstName', 'FrontTest');
     I.click({id: 'apply'});
     I.wait(5);
-    I.dontSee('2A');
+    I.dontSee('11111111111');
+    I.click({id: 'filter_users'});
+    I.clearField('firstName');
+    I.fillField('firstName', 'Front1Test');
+    I.click({id: 'apply'});
+    I.wait(5);
+    I.see('11111111111');
 
     //reset filters
     I.click({id: 'filter_users'});
     I.click({id: 'reset'});
-    I.see('2A');
 });
 
 Scenario('can change displayed columns', ({I}) => {
     //login
-    //I.amOnPage('http://school-management-system.online:24020');
-    I.amOnPage('http://localhost:24010/');
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
     I.click({id: 'open_drawer'});
 
     //login
@@ -184,4 +189,84 @@ Scenario('can change displayed columns', ({I}) => {
     I.see('E-mail');
     I.see('Phone');
     I.see('ID');
+});
+
+Scenario('can delete student', ({I}) => {
+    //login
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
+    I.click({id: 'open_drawer'});
+
+    //login
+    I.fillField('username', 'smsadmin');
+    I.fillField('password', 'smsadmin');
+    I.click('Sign In');
+
+    //go to student management
+    I.see('DASHBOARD FOR ADMIN');
+    I.click({id: 'open_drawer'});
+    I.click('Student management');
+    I.see('Student management');
+
+    //delete user
+    I.click({id: 'filter_users'});
+    I.fillField('firstName', 'Front1Test');
+    I.click({id: 'apply'});
+    I.see('Front1TestLN');
+    I.click({id: 's_11111111111'});
+    I.see('Personal information');
+    I.click({id: 'delete'});
+    I.click({id: 'yes'});
+});
+
+Scenario('can delete group', ({I}) => {
+    //login
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
+    I.click({id: 'open_drawer'});
+
+    //login
+    I.fillField('username', 'smsadmin');
+    I.fillField('password', 'smsadmin');
+    I.click('Sign In');
+
+    //go to student management
+    I.see('DASHBOARD FOR ADMIN');
+    I.click({id: 'open_drawer'});
+    I.click('Student management');
+    I.see('Student management');
+
+    //delete group
+    I.click({id: "manage_groups"});
+    I.see('TEST_FG');
+    I.click({id: "delete_TEST_FG"});
+    I.wait(5);
+    I.dontSee('TEST_FG');
+});
+
+Scenario('can create teacher', ({I}) => {
+    //login
+    I.amOnPage('http://school-management-system.online:24020');
+    //I.amOnPage('http://localhost:24010/');
+    I.click({id: 'open_drawer'});
+
+    I.fillField('username', 'smsadmin');
+    I.fillField('password', 'smsadmin');
+    I.click('Sign In');
+
+    //go to student management
+    I.see('DASHBOARD FOR ADMIN');
+    I.click({id: 'open_drawer'});
+    I.click('Teacher management');
+    I.see('Teacher management');
+
+    //create teacher
+    I.click({id: "add_user"});
+    I.see('Create new teacher');
+    I.fillField('firstName', 'testTeacher');
+    I.fillField('lastName', 'testTeacher');
+    I.fillField('pesel', '88888888888');
+    I.fillField('email', 'testteacher@sms.com');
+    I.click('Submit');
+
 });
